560c347485573b7458e1e7c79e97708bbf11201b
Ignaciofmunoz <Ignacio40309782@yahoo.com>
2018-03-02 02:54:30 -0300

    Update stacktest.cpp

--- continua/stacktest.cpp
+++ continua/stacktest.cpp
@@ -1,4 +1,6 @@
+
 #include <iostream>
+#include <cassert>
 #include "stack.h"
 
 using namespace std;
@@ -7,31 +9,37 @@ using namespace std;
 
 int main()
 {
-Carta aux;
-cout<<"Esta es la prueba para la estructura de datos Pila (LIFO) "<<endl<<"en este caso para resolver cuestiones referidas a cartas españolas"<<endl;
-Stack* S=NULL;
+    using namespace Stack;
+	//using ::Stack::Nodo;
+
+    Stack S;
+cout<<"Esta es la prueba para la estructura de datos Pila (LIFO) "<<endl<<"en este caso para resolver problemas con cartas españolas"<<endl;
+
     cout<<"se inicia la pila (vacia)"<<endl;
-    //coutstack(S);
-    cout<<"se cargan las cartas 1 de basto , 5 de copa ,9 de espada y 12 de oro"<<endl<<endl;
+    cout<<"se cargan las cartas: 1 de basto , 5 de copa ,9 de espada y 12 de oro"<<endl<<endl;
     push(S,"basto",1);
+    push(S,"copa",5);
+    assert(pop(S).numero==5);
     push(S,"copa",5);
     push(S,"espada",9);
     push(S,"oro",12);
     coutstack(S);
 
 
-       // while (isempty(S)==false)
-        while (S)
+/*
+        while (isempty(s)==false)
     {
 
         cout<<"Se retira la ultima de la pila:\t"<<endl;
-        aux =pop(S);
+        Carta aux =pop(S);
       cout<<aux.numero <<" de "<<aux.palo<<endl<<endl;
       cout<<"Se muestran las cartas del mazo"<<endl<<endl;
         coutstack(S);
-
     }
+    assert(isempty(s)==true);*/
  return 0;
 
 
 }
+
+
