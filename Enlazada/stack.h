6cf6f5014f83399e551982977ee0ea166615bb7f
Ignaciofmunoz <Ignacio40309782@yahoo.com>
2018-03-05 02:06:58 -0300

    Update stack.h

--- Enlazada/stack.h
+++ Enlazada/stack.h
@@ -1,7 +1,7 @@
-
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
 #include <array>
+using namespace std;
 
 namespace Stack{
 struct Carta {
@@ -11,15 +11,14 @@ std::string palo;
 struct Nodo
 {
     Carta c;
-
     Nodo *next;
 };
-typedef Nodo *Stack;
-void push(Stack&,std::string palo,int num);
-Carta pop(Stack& );
-void coutstack(Stack& );
-void nullstack(Stack& );
-bool isempty(Stack& );
+struct Stack{Nodo *cima;};
+void push(Nodo&,&Carta);
+Carta pop(Nodo&);//cambie todo los stack por nodo
+void coutstack(Nodo& );
+void nullstack(Nodo& );
+bool isempty(Nodo& );
+void InitStacks(Nodo&)
 }
-#endif // STACK_H_INCLUDED*/
-
+#endif // STACK_H_INCLUDED
