96b68898c6dd469cd5829ab0d0dbd3c893555e7c
Ignaciofmunoz <Ignacio40309782@yahoo.com>
2018-03-07 18:55:01 -0300

    Update stacktest.cpp

--- continua/stacktest.cpp
+++ continua/stacktest.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <cassert>
-#include "stack.h"
-
+#include "stack.h" solo en la entrega en git
 using namespace std;
-
+using namespace Stack;
 void InitCartas(Stack::Carta&b,Stack::Carta&c,Stack::Carta&e,Stack::Carta&o)
 {
     b.numero=1;
@@ -18,15 +17,14 @@ void InitCartas(Stack::Carta&b,Stack::Carta&c,Stack::Carta&e,Stack::Carta&o)
 
 int main()
 {
-    using namespace Stack;
-	using ::Stack::Stack;
-Stack S;
-InitStack(S);//no hace nada, para que queden igual los archivos
+Stack::Stack S;// esta es la que tengo en la continua
+//Nodo*S;//para la enlazada
 Carta b,c,e,o;
 InitCartas(b,c,e,o);
 cout<<"Esta es la prueba para la estructura de datos Pila (LIFO) "<<endl<<"en este caso para resolver problemas con cartas españolas"<<endl;
     cout<<"se inicia la pila (vacia)"<<endl;
     cout<<"se cargan las cartas: 1 de basto , 5 de copa ,9 de espada y 12 de oro"<<endl<<endl;
+
     push(S,b);
     push(S,c);
     assert(pop(S).numero==5);
@@ -40,7 +38,8 @@ cout<<"Esta es la prueba para la estructura de datos Pila (LIFO) "<<endl<<"en es
     assert(pop(S).palo=="oro");
     push(S,o);
     coutstack(S);
-        while (not isempty(S))
+        while (not isempty(S))//no andaria funcando arreglarlo
+
     {
         cout<<"Se retira la ultima de la pila:\t"<<endl;
         Carta aux =pop(S);
