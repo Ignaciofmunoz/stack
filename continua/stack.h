e731801819154615834ab189ca5df6b84cc8b271
Ignaciofmunoz <Ignacio40309782@yahoo.com>
2018-03-07 18:52:31 -0300

    Update stack.h

--- continua/stack.h
+++ continua/stack.h
@@ -1,26 +1,25 @@
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
 #include <array>
+#include <string>
 
 using namespace std;
 
 namespace Stack{
 struct Carta {
 int numero;
-std::string palo;
+string palo;
 };
-struct Nodo
+struct Stack
 {
  std::array<Carta ,100000>a;
     int i=0;
 };
-typedef Nodo Stack;//solo compila si lo agrego
 
 Carta  pop (Stack&);
-void push (Stack&,Carta);
+void push (Stack&,Carta&);
 bool isempty (Stack&);
 void coutstack(Stack&);
 void InitStack(Stack&);
 }
 #endif // STACKCONT_H_INCLUDED
-
