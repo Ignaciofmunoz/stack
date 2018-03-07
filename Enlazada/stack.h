9886196adae4728ad11bb08e6a85bfc3a9d97a9f
Ignaciofmunoz <Ignacio40309782@yahoo.com>
2018-03-07 18:42:49 -0300

    Update stack.h

--- Enlazada/stack.h
+++ Enlazada/stack.h
@@ -1,6 +1,7 @@
 #ifndef STACKPOINTER_H_INCLUDED
 #define STACKPOINTER_H_INCLUDED
 #include <array>
+#include <string>
 using namespace std;
 
 namespace Stack{
@@ -13,13 +14,13 @@ struct Nodo
     Carta c;
     Nodo *next;
 };
-struct Stack{Nodo *cima;};
+struct Stack{Nodo *cima;};//no le logro dar uso
 
-void push(Nodo&,Carta&);
-Carta pop(Nodo&);//cambie todo los stack por nodo
-void coutstack(Nodo& );
-void nullstack(Nodo& );
-bool isempty(const Nodo& );
-void InitStacks(const Nodo&);
+void push(Nodo* &,Carta&);
+Carta pop(Nodo *&);//quizas desp cambiarlos para ver q onda con la nueva estructura
+void coutstack(Nodo *& );
+//void nullstack(Nodo& );
+bool isempty( const Nodo * );//ver si sirve dejar el const
+//void InitStack(Nodo *&);
 }
 #endif // STACKPOINTER_H_INCLUDED
