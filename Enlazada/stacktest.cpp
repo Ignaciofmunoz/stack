de5ff3700e0a16928d1947a30768ee02786d6dc4
Ignaciofmunoz <Ignacio40309782@yahoo.com>
2018-03-08 03:08:44 -0300

    Update stacktest.cpp

--- Enlazada/stacktest.cpp
+++ Enlazada/stacktest.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <cassert>
 #include "stack.h"
+
 using namespace std;
 using namespace Stack;
-void InitCartas(Stack::Carta&b,Stack::Carta&c,Stack::Carta&e,Stack::Carta&o)
+Stack::Stack S;
+Stack::Carta b,c,e,o;
+void InitCartas(Carta&b,Carta&c,Carta&e,Carta&o)
 {
     b.numero=1;
     b.palo="palo";
@@ -17,17 +20,17 @@ void InitCartas(Stack::Carta&b,Stack::Carta&c,Stack::Carta&e,Stack::Carta&o)
 
 int main()
 {
-//Stack::Stack S; esta es la que tengo en la continua
-Nodo*S;//esta en la enlazada
-Carta b,c,e,o;
+
 InitCartas(b,c,e,o);
 cout<<"Esta es la prueba para la estructura de datos Pila (LIFO) "<<endl<<"en este caso para resolver problemas con cartas españolas"<<endl;
     cout<<"se inicia la pila (vacia)"<<endl;
     cout<<"se cargan las cartas: 1 de basto , 5 de copa ,9 de espada y 12 de oro"<<endl<<endl;
+
     push(S,b);
     push(S,c);
     assert(pop(S).numero==5);
     push(S,c);
+
     assert(pop(S).palo=="copa");
     push(S,c);
     push(S,e);
@@ -37,8 +40,8 @@ cout<<"Esta es la prueba para la estructura de datos Pila (LIFO) "<<endl<<"en es
     assert(pop(S).palo=="oro");
     push(S,o);
     coutstack(S);
-    
-        while (not isempty(S))
+       while (not isempty(S))
+
     {
         cout<<"Se retira la ultima de la pila:\t"<<endl;
         Carta aux =pop(S);
